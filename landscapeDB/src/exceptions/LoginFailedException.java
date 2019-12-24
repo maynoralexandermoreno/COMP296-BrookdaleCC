80bb6195e6e54b9418d0e488ccdc1d5c7263c1d5
Alex <Alex>
2019-12-24 11:42:42 -0500

    Final Capstone Project From 2018

---
+++ landscapeDB/src/exceptions/LoginFailedException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+/*
+ * Custom exception for Login.java
+ */
+
+public class LoginFailedException extends Exception {
+	
+	public LoginFailedException(String ex) {
+		super(ex);
+	}
+	public String getMessage() {
+		return super.getMessage();
+	}
+}
---
+++ landscapeDB/src/login/Main.java
@@ -0,0 +1,27 @@
+package login;
+// Necessary Imports
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
+
+// Starts program
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("landscapeDB Login");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
